0e03d4e3793e7484df2486501d84c08f82f5c996
moge <imaamy01@gmail.com>
2023-04-24 18:09:21 +0300

    adsense added

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9086453152549847"
+     crossOrigin="anonymous"></script>
+      </head>
       <body className='bg-slate-200 overflow-x-hidden'>
       <Providers>
         <Navbar />
