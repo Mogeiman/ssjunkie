68df11bc1fca5baa2692e68e35fe62a338e949f6
moge <imaamy01@gmail.com>
2023-04-25 11:50:43 +0300

    monetag added

--- app/chapters/[id]/loading.tsx
+++ app/chapters/[id]/loading.tsx
@@ -1,9 +1,10 @@
 export default function Loading () {
     return(
-        <div>
+      <div className="w-full md:w-4/5 lg:w-3/5 h-full bg-slate-50 mx-auto landscape:w-full">
+      <ul className="w-5/6 h-full mx-auto list-none">
         {[...Array(20).keys()].map((i) => {
           return (
-            <li key={i}>
+            <li className="w-full" key={i}>
               <span
                 className="inline-block h-5 animate-pulse w-full bg-slate-500"
                 style={{
@@ -14,7 +15,8 @@ export default function Loading () {
             </li>
           );
         })}
-      </div>    
+      </ul>    
+      </div>
       )
 }
 
