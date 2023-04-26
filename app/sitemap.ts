4d1ebaa29399365d2bbc77282e36e33e440c16fe
moge <imaamy01@gmail.com>
2023-04-26 11:07:14 +0300

    sitemap added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,13 @@
-// import { MetadataRoute } from 'next';
-// import { useSelector } from 'react-redux';
-// import { RootState } from './redux/store';
-// export default function sitemap(): MetadataRoute.Sitemap {
-//     const chapters = useSelector((state: RootState) => state?.chapter?.data);
+import { MetadataRoute } from 'next';
+import { useSelector } from 'react-redux';
+import { RootState } from './redux/store';
+export default function sitemap(): MetadataRoute.Sitemap {
+    const chapters = useSelector((state: RootState) => state?.chapter?.data);
 
-//     const sitemapUrls = chapters?.map((chapter) => ({
-//         url: `https://wwww.shadowslavejunkie.online/chapters/${chapter.id}`,
-//         lastModified: new Date(),
-//       }));
-//       console.log(sitemapUrls)
-//   return sitemapUrls;
-// }
+    const sitemapUrls = chapters?.map((chapter) => ({
+        url: `https://wwww.shadowslavejunkie.online/chapters/${chapter.id}`,
+        lastModified: new Date(),
+      }));
+      console.log(sitemapUrls)
+  return sitemapUrls;
+}
