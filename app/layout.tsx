850b33ea802447ab89da3bcc747ab66cabbd84b7
moge <imaamy01@gmail.com>
2023-09-26 21:45:45 +0300

    added next-google-adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import
 from
  
 '@vercel/analytics/react'
+import { GoogleAdSense } from "nextjs-google-adsense";
+
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer'
 import Providers from './redux/provider'
@@ -20,13 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9086453152549847"
-     crossOrigin="anonymous"></script>
+      {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9086453152549847"
+     crossOrigin="anonymous"></script> */}
      <meta name="google-site-verification" content="ZXDfvzdLVT69YhEctmWOt4Buhnjt5sZ1I8mVkofTLkM" />
      
       </head>
       <body className='bg-slate-200 overflow-x-hidden'>
       <Providers>
+        <GoogleAdSense publisherId="pub-5544517639382721" />
         <Navbar />
         {children}
         <Analytics />
